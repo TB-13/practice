21471886ecd9a4872177bbf6b503f36fe058248d
TB-13 <tahabouhlal23@gmail.com>
2023-12-12 17:03:16 +0100

    login screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl,FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'practice';
+  //Form Validables
+  registerForm:any = FormGroup;
+  submitted = false;
+  constructor( private formBuilder: FormBuilder){}
+//Add user form actions
+  get f() { return this.registerForm.controls; }
+  onSubmit() {
+
+    this.submitted = true;
+    // stop here if form is invalid
+    if (this.registerForm.invalid) {
+      return;
+    }
+    //True if all the fields are filled
+    if(this.submitted)
+    {
+      alert("Great!!");
+    }
+
+  }
+  ngOnInit() {
+    //Add User form validations
+    this.registerForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
+    });
+  }
 }
