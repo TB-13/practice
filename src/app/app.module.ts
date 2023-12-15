5528e4b85c2444438d98a42a4b65966a897d845d
TB-13 <tahabouhlal23@gmail.com>
2023-12-15 14:59:48 +0100

    login screen update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { LoginModule } from "./login/login.module";
-import { MainPageModule } from "./main-page/main-page.module";
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {MainPageModule} from "./main-page/main-page.module";
+import { SidenavComponent } from './sidenav/sidenav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    SidenavComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
     LoginModule,
+    FontAwesomeModule,
     MainPageModule
   ],
   providers: [],
+  exports: [
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
